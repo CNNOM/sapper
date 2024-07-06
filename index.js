4e4934e44d2b928965c8ae67ec1f88bd586b103a
Алексей Зенков <zenkov.m700@mail.ru>
2024-07-06 18:22:34 +0300

    Добавлено отображение времени и количество оставшихся мин

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+// Constants
+const COLS = 8;
+const ROWS = 8;
+const MINES = 10;
+
+// State
 let arr = [];
-let cols = 8;
-let rows = 8;
-let mines = 10;
 let revealed = {};
 let flagged = {};
 let firstClick = true;
 let firstClickRow = -1;
 let firstClickCol = -1;
-const body = document.querySelector("body")
 let flagsCount = 0;
-let countMines = mines;
-let countNum = cols * rows - mines;
-
-
+let countNum = COLS * ROWS - MINES;
 let startTime = Date.now();
-let timerElement = document.getElementsByName('timer');
+let timerElement;
 let formattedTime;
+
+// Colors mapping
 const colors = {
     0: "white",
     1: "blue",
@@ -28,6 +29,10 @@ const colors = {
     8: "gray"
 };
 
+// DOM Elements
+const body = document.querySelector("body");
+
+// Functions
 function updateTimer() {
     let currentTime = Date.now();
     let elapsedTime = currentTime - startTime;
@@ -45,92 +50,76 @@ function updateTimer() {
 function printArrHtml() {
     let minCount = document.createElement("div");
     minCount.className = "min_count";
-    minCount.textContent = `${countMines}`
+    minCount.textContent = `${MINES}`;
 
-    let timer = document.createElement("div");
-    timer.className = "timer";
-    timer.id = "timer";
-    timer.textContent = `00:00:00`
-    document.body.appendChild(timer);
+    timerElement = document.createElement("div");
+    timerElement.className = "timer";
+    timerElement.id = "timer";
+    timerElement.textContent = "00:00:00";
+
+    body.appendChild(minCount);
+    body.appendChild(timerElement);
 
     startTime = Date.now();
-    timerElement = document.getElementById('timer');
-
     setInterval(updateTimer, 1000);
 
-
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < ROWS; i++) {
         let rowDiv = document.createElement("div");
         rowDiv.className = "minesweeper_row";
 
-        for (let j = 0; j < cols; j++) {
+        for (let j = 0; j < COLS; j++) {
             let cellDiv = document.createElement("button");
-            cellDiv.className = `minesweeper_field fieledNum}`;
-            cellDiv.id = `cell_${i}_${j}`
-            revealed[`${i}${j}`] = false
-            flagged[`${i}${j}`] = false
+            cellDiv.className = "minesweeper_field fieledNum";
+            cellDiv.id = `cell_${i}_${j}`;
+            revealed[`${i}${j}`] = false;
+            flagged[`${i}${j}`] = false;
 
             cellDiv.addEventListener('mousedown', function (event) {
-                if (event.button === 0 && !(flagged[`${i}${j}`] || revealed[`${i}${j}`])) {
-                    if (firstClick) {
-                        firstClickRow = i;
-                        firstClickCol = j;
-                        firstClick = false;
-                        console.log(firstClickRow, firstClickCol)
-                        arrSapper()
-                    }
-                    revealed[`${i}${j}`] = true;
-                    revealCells(i, j)
-                }
-                if (event.button === 2 && !revealed[`${i}${j}`] && countMines > 0) {
+                handleCellClick(event, i, j);
+            });
 
-                    flagged[`${i}${j}`] = !flagged[`${i}${j}`];
-
-                    if (flagged[`${i}${j}`]) {
-                        cellDiv.className = `minesweeper_field flag`;
-                        flagsCount++;
-                    } else {
-                        cellDiv.className = `minesweeper_field fieledNum}`;
-                        flagsCount--;
-                    }
-                    countMines = mines - flagsCount;
-                    console.log(countMines)
-                    minCount.textContent = `${countMines}`
-
-                }
-
-            })
             cellDiv.addEventListener('contextmenu', function (event) {
                 event.preventDefault();
             });
+
             rowDiv.appendChild(cellDiv);
-
-
         }
 
         body.appendChild(rowDiv);
-        body.appendChild(minCount);
-        document.body.appendChild(timer);
-
     }
 }
 
+function handleCellClick(event, i, j) {
+    if (event.button === 0 && !(flagged[`${i}${j}`] || revealed[`${i}${j}`])) {
+        if (firstClick) {
+            firstClickRow = i;
+            firstClickCol = j;
+            firstClick = false;
+            arrSapper();
+        }
+        revealed[`${i}${j}`] = true;
+        revealCells(i, j);
+    } else if (event.button === 2 && !revealed[`${i}${j}`] && MINES - flagsCount > 0) {
+        flagged[`${i}${j}`] = !flagged[`${i}${j}`];
+        if (flagged[`${i}${j}`]) {
+            flagsCount++;
+            document.getElementById(`cell_${i}_${j}`).classList.add('flag');
+        } else {
+            flagsCount--;
+            document.getElementById(`cell_${i}_${j}`).classList.remove('flag');
+        }
+        document.querySelector('.min_count').textContent = `${MINES - flagsCount}`;
+    }
+}
 
 function arrSapper() {
-
-    for (let i = 0; i < rows; i++) {
-        arr[i] = [];
-        for (let j = 0; j < cols; j++) {
-            arr[i][j] = "0";
-        }
-    }
+    arr = Array.from({ length: ROWS }, () => Array(COLS).fill("0"));
 
     let count = 0;
-    while (count < mines) {
-        const row = Math.floor(Math.random() * rows);
-        const col = Math.floor(Math.random() * cols);
-        console.log('ввввввввв', firstClickRow, firstClickCol)
-        if ((row != firstClickRow || col != firstClickCol) &&
+    while (count < MINES) {
+        const row = Math.floor(Math.random() * ROWS);
+        const col = Math.floor(Math.random() * COLS);
+        if ((row !== firstClickRow || col !== firstClickCol) &&
             (row < firstClickRow - 1 || row > firstClickRow + 1 || col < firstClickCol - 1 || col > firstClickCol + 1)) {
             if (arr[row][col] !== "*") {
                 arr[row][col] = "*";
@@ -138,22 +127,15 @@ function arrSapper() {
             }
         }
     }
-    // printArr()
 
-    let countMines;
-    let x = 0;
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            countMines = 0;
-
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
             if (arr[i][j] !== "*") {
-                for (let x = Math.max(0, i - 1); x <= Math.min(i + 1, rows - 1); x++) {
-                    for (let y = Math.max(0, j - 1); y <= Math.min(j + 1, cols - 1); y++) {
-
-                        if (x !== i || y !== j) {
-                            if (arr[x][y] === "*") {
-                                countMines++;
-                            }
+                let countMines = 0;
+                for (let x = Math.max(0, i - 1); x <= Math.min(i + 1, ROWS - 1); x++) {
+                    for (let y = Math.max(0, j - 1); y <= Math.min(j + 1, COLS - 1); y++) {
+                        if (arr[x][y] === "*") {
+                            countMines++;
                         }
                     }
                 }
@@ -164,7 +146,7 @@ function arrSapper() {
 }
 
 function revealCells(i, j) {
-    if (i < 0 || i >= rows || j < 0 || j >= cols) return;
+    if (i < 0 || i >= ROWS || j < 0 || j >= COLS) return;
 
     let cell = document.getElementById(`cell_${i}_${j}`);
     let value = arr[i][j];
@@ -173,7 +155,7 @@ function revealCells(i, j) {
 
     printNum(value, cell);
     countNum--;
-    console.log(countNum)
+
     cell.classList.add('revealed');
 
     if (value === "0") {
@@ -187,34 +169,25 @@ function revealCells(i, j) {
     }
 
     if (countNum === 0) {
-        alert(`Вы выиграли! Игровое время ${formattedTime}`)
-        location.reload()
+        alert(`Вы выиграли! Игровое время ${formattedTime}`);
+        location.reload();
     }
 }
 
 function printNum(value, cellDiv) {
-
-    cellDiv.textContent = `${value}`
+    cellDiv.textContent = value;
     let num = colors[value];
 
     if (value === "*") {
         cellDiv.className = `minesweeper_field mines`;
-        alert(`Вы проиграли! Игровое время ${formattedTime}`)
-        location.reload()
-
+        alert(`Вы проиграли! Игровое время ${formattedTime}`);
+        location.reload();
     } else {
-        if (value !== "0") {
-            cellDiv.className = `minesweeper_field fieledNum ${num}`;
-
-        } else {
-            cellDiv.className = `minesweeper_field fieledNum ${num}`;
-            cellDiv.textContent = ` `
-
+        cellDiv.className = `minesweeper_field fieledNum ${num}`;
+        if (value === "0") {
+            cellDiv.textContent = "";
         }
     }
 }
 
-printArrHtml()
-
-
-
+printArrHtml();
